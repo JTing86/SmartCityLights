66dfea5986cd00c5de86201f99981db289d7cc56
JTing86 <jingting9786@gmail.com>
2018-11-10 21:22:52 -0500

    Implemented dim lightings

--- lightstrips/lightstrips.ino
+++ lightstrips/lightstrips.ino
@@ -6,25 +6,29 @@
 #define PIN 6 //PIN connected on arduino
 #define num_leds 23
 
-int len = 5;
-//int input_leds[5]={1,1,0,0,1};   // for incoming serial data
-//int input_bytes[5] = {};
+int len = 6;
+//int input_bytes[6] = {};
 String input;
+
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+uint32_t low = strip.Color(30, 18, 0); 
 
 void setup() {
   Serial.begin(9600); 
+  // dim
+  for( int i = 0; i<len; i++){
+      strip.setPixelColor(i, low);
+      strip.show();
+  }  
   strip.begin();
   strip.show(); 
 }
 
-
 void loop() {
-
+      
   while (Serial.available() != 0) {
-
     input = Serial.readString();
-
     turn_on_led_chain(input);
   } 
 }
@@ -42,20 +46,16 @@ void turn_on_led_chain(String input_bytes){
     for (int i=0; i<len; i++){
      
       if (input_bytes[i] == '1'){
-        strip.setPixelColor(i, 0, 200, 200);
+        strip.setPixelColor(i, 255, 120, 0);
         strip.show();
       }
       else if (input_bytes[i] == '0'){
-        strip.setPixelColor(i, 0, 0, 0);
+        strip.setPixelColor(i, 30, 18, 0);
         strip.show();
       }  
       else{
-        strip.setPixelColor(i, 200, 0, 0);
+        strip.setPixelColor(i, 30, 18, 0);
         strip.show();
       }
      }
-}
-
-void follow_up_led_chain_action(int rate){
-  
 }
