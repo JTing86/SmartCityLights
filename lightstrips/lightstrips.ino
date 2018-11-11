2aae23a6ce3307a5410a83ef970f191cfd0b7eb0
JTing86 <jingting9786@gmail.com>
2018-11-10 20:36:40 -0500

    Changed to accept binary string from Serial

--- lightstrips/lightstrips.ino
+++ lightstrips/lightstrips.ino
@@ -6,49 +6,53 @@
 #define PIN 6 //PIN connected on arduino
 #define num_leds 23
 
-int input_leds[5]={};   // for incoming serial data
-int input_bytes[60] = {};
-//int LED_array[5];
-//int input_leds[5];
-
+int len = 5;
+//int input_leds[5]={1,1,0,0,1};   // for incoming serial data
+//int input_bytes[5] = {};
+String input;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600); 
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show(); 
 }
 
 
 void loop() {
 
   while (Serial.available() != 0) {
-    for (int i=0;i<60;i++){
-      input_bytes[60] = Serial.read();
-    }
-    turn_on_led_chain(input_bytes);
-  }     
+
+    input = Serial.readString();
+
+    turn_on_led_chain(input);
+  } 
 }
 
 void turn_on_led(int n){
   //fill_solid( leds, 60, CRGB(50,0,200));
+  strip.setPixelColor(n, 0, 200, 200);
   strip.show();
-  delay(3000); 
-  strip.setPixelColor(n, 0, 0, 0);
-  strip.show();
+  //delay(3000); 
+  //strip.setPixelColor(n, 0, 0, 0);
+  //strip.show();
 }
 
-void turn_on_led_chain(int LED_array[]){
-    for (int i=0; i<60; i++){
+void turn_on_led_chain(String input_bytes){
+    for (int i=0; i<len; i++){
      
-      if (LED_array[i] == 1){
+      if (input_bytes[i] == '1'){
         strip.setPixelColor(i, 0, 200, 200);
         strip.show();
       }
-      else if (LED_array[i] == 0){
+      else if (input_bytes[i] == '0'){
         strip.setPixelColor(i, 0, 0, 0);
         strip.show();
       }  
+      else{
+        strip.setPixelColor(i, 200, 0, 0);
+        strip.show();
+      }
      }
 }
 
